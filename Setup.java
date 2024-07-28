15e0437f0d103c6eeb97e3ce9ed0e568fc9407bf
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-07-28 21:32:16 +0300

    Moved Scripts to builders

--- Setup.java
+++ Setup.java
@@ -5,59 +5,65 @@ import java.io.InputStreamReader;
 
 @SuppressWarnings("deprecation")
 public class Setup {
-    public static void main(String[] args) {
-        String os = System.getProperty("os.name").toLowerCase();
-        String osVersion = System.getProperty("os.version");
-        String osArch = System.getProperty("os.arch");
+  public static void main(String[] args) {
+    String os = System.getProperty("os.name").toLowerCase();
+    String osVersion = System.getProperty("os.version");
+    String osArch = System.getProperty("os.arch");
 
-        // Suppressed println statements for cleaner output
-        // System.out.println("Operating System: " + os);
-        // System.out.println("OS Version: " + osVersion);
-        // System.out.println("OS Architecture: " + osArch);
+    // Suppressed println statements for cleaner output
+    // System.out.println("Operating System: " + os);
+    // System.out.println("OS Version: " + osVersion);
+    // System.out.println("OS Architecture: " + osArch);
 
-        try {
-            if (os.contains("win")) {
-                // Windows-specific code
-                String scriptPath = "run.bat";
-                String command = "cmd /c \"" + scriptPath + "\"";
-                runCommand(command);
-            } else if (os.contains("nix") || os.contains("nux")) {
-                // Unix/Linux-specific code
-                String scriptPath = "run.sh";
-                String command = "sh \"" + scriptPath + "\"";
-                runCommand(command);
-            } else if (os.contains("mac")) {
-                // macOS-specific code
-                String scriptPath = "run.sh";
-                String command = "sh \"" + scriptPath + "\"";
-                runCommand(command);
-            } else {
-                System.out.println("Unknown operating system.");
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    try {
+      String scriptPath;
+      String command;
+
+      if (os.contains("win")) {
+        // Windows-specific code
+        scriptPath = "config\\builders\\run.bat";
+        command = "cmd /c \"" + scriptPath + "\"";
+      } else if (os.contains("nix") || os.contains("nux")) {
+        // Unix/Linux-specific code
+        scriptPath = "config/builders/run.sh";
+        command = "sh \"" + scriptPath + "\"";
+      } else if (os.contains("mac")) {
+        // macOS-specific code
+        scriptPath = "config/builders/run.sh";
+        command = "sh \"" + scriptPath + "\"";
+      } else {
+        System.out.println("Unknown operating system.");
+        return; // Exit if the OS is unknown
+      }
+
+      runCommand(command);
+
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void runCommand(String command) throws IOException, InterruptedException {
+    Process process = Runtime.getRuntime().exec(command);
+
+    // Capture and display the output
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
     }
 
-    private static void runCommand(String command) throws IOException, InterruptedException {
-        Process process = Runtime.getRuntime().exec(command);
+    int exitCode = process.waitFor();
 
-        // Capture and display the output
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
-
-        int exitCode = process.waitFor();
-
-        if (exitCode == 0) {
-            // Delete the Setup.class file after the script completes
-            File classFile = new File("Setup.class");
-            classFile.delete();
-        } else {
-            System.out.println("Script execution failed with exit code: " + exitCode);
-        }
+    if (exitCode == 0) {
+      // Delete the Setup.class file after the script completes
+      File classFile = new File("Setup.class");
+      if (classFile.exists()) {
+        classFile.delete();
+      }
+    } else {
+      System.out.println("Script execution failed with exit code: " + exitCode);
     }
+  }
 }
--- app/Main.java
+++ app/Main.java
@@ -16,97 +16,94 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main extends Application {
+  private static Connection connection;
 
-    private static Connection connection;
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("/views/addPassenger.fxml"));
+      Scene scene = new Scene(root);
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Ajouter un passager");
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/add_passenger.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Ajouter un passager");
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+  @Override
+  public void stop() {
+    // Close the database connection when the application is stopped
+    closeDatabaseConnection();
+  }
+
+  public static void main(String[] args) {
+    connection = initializeDatabase();
+    createDatabaseSchema();
+    launch(args);
+  }
+
+  private static Connection initializeDatabase() {
+    String url = "jdbc:mysql://localhost:3306/";
+    String user = "root";
+    String password = "";
+
+    Connection connection = null;
+
+    try {
+      // Load the MySQL JDBC driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      // Establish the connection
+      connection = DriverManager.getConnection(url, user, password);
+      System.out.println("Database connected successfully!");
+
+    } catch (ClassNotFoundException e) {
+      System.out.println("MySQL JDBC Driver not found.");
+      e.printStackTrace();
+    } catch (SQLException e) {
+      System.out.println("Connection failed.");
+      e.printStackTrace();
+    }
+
+    return connection;
+  }
+
+  private static void closeDatabaseConnection() {
+    if (connection != null) {
+      try {
+        connection.close();
+        System.out.println("Database connection closed.");
+      } catch (SQLException e) {
+        System.out.println("Failed to close the connection.");
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static void createDatabaseSchema() {
+    String sqlScriptPath = "config/database/database.sql";
+
+    try (BufferedReader br = new BufferedReader(new FileReader(sqlScriptPath));
+      Statement stmt = connection.createStatement()) {
+
+      String line;
+      StringBuilder sql = new StringBuilder();
+
+      while ((line = br.readLine()) != null) {
+        sql.append(line).append("\n");
+      }
+
+      String[] sqlCommands = sql.toString().split(";");
+
+      for (String command : sqlCommands) {
+        if (!command.trim().isEmpty()) {
+          stmt.execute(command.trim());
         }
+      }
+      System.out.println("Database schema created successfully.");
+    } catch (IOException | SQLException e) {
+      e.printStackTrace();
     }
-
-    @Override
-    public void stop() {
-        // Close the database connection when the application is stopped
-        closeDatabaseConnection();
-    }
-
-    public static void main(String[] args) {
-        connection = initializeDatabase();
-        createDatabaseSchema();
-        launch(args);
-    }
-
-    private static Connection initializeDatabase() {
-        String url = "jdbc:mysql://localhost:3306/";
-        String user = "root";
-        String password = "";
-
-        Connection connection = null;
-
-        try {
-            // Load the MySQL JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Establish the connection
-            connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Database connected successfully!");
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.out.println("Connection failed.");
-            e.printStackTrace();
-        }
-
-        return connection;
-    }
-
-    private static void closeDatabaseConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-                System.out.println("Database connection closed.");
-            } catch (SQLException e) {
-                System.out.println("Failed to close the connection.");
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private static void createDatabaseSchema() {
-        String sqlScriptPath = "config/database/database.sql";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(sqlScriptPath));
-             Statement stmt = connection.createStatement()) {
-
-            String line;
-            StringBuilder sql = new StringBuilder();
-
-            while ((line = br.readLine()) != null) {
-                sql.append(line).append("\n");
-            }
-
-            String[] sqlCommands = sql.toString().split(";");
-
-            for (String command : sqlCommands) {
-                if (!command.trim().isEmpty()) {
-                    stmt.execute(command.trim());
-                }
-            }
-
-            System.out.println("Database schema created successfully.");
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
