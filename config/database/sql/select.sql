ebe9c4603512b733cd980a2ada844297f3fbc0a5
r$h <randrianirinahernandezstephan@gmail.com>
2024-07-29 09:52:28 +0300

    Query select added

---
+++ config/database/sql/select.sql
@@ -0,0 +1,15 @@
+
+ ---Admin Tab--
+ SELECT Username AS "Nom d'utilisateur",AdmnNom AS Nom,AdmnPrenom AS Prenom FROM admin;
+
+ --Notam Passager ---
+ SELECT NumPssrt AS Passeport,PassNom AS Nom ,PassPrenom AS Prenom ,Naissance AS "Date de naissance" FROM passager WHERE Notam=TRUE ORDER BY PassNom;
+ --Iste Passager --
+ SELECT NumPssrt AS Passeport,PassNom AS Nom ,PassPrenom AS Prenom ,Naissance AS "Date de naissance" FROM passager WHERE Iste=TRUE ORDER BY PassNom;
+--Simple Passager--
+ SELECT NumPssrt AS Passeport,PassNom AS Nom ,PassPrenom AS Prenom ,Naissance AS "Date de naissance" FROM passager WHERE Notam=FALSE AND Iste=FALSE ORDER BY PassNom;
+
+ --Select Vol --
+SELECT NumVol AS Numero,Origine AS Provenance,Destination AS Destinatioin ,Avion AS Avion,DATE_FORMAT(Decolage, '%Y-%m-%d %H:%i:%s') AS Decolage FROM vol WHERE DateVol >= NOW() ORDER BY DateVol ASC;
+
+
