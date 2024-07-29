dd6d1b2194251dfeec55a8ed6c643f2bd96dfe9e
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-07-29 04:45:49 +0300

    Added Pages

--- controllers/DashboardController.java
+++ controllers/DashboardController.java
@@ -1,3 +1,49 @@
 package app.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
 
+public class DashboardController {
+
+    @FXML
+    private Pane contentPane;
+
+    @FXML
+    private void initialize() {
+        showReservationPage(); // Load the default page
+    }
+
+    @FXML
+    private void showReservationPage() {
+        loadPage("pageReservations.fxml");
+    }
+
+    @FXML
+    private void showVolsPage() {
+        loadPage("pageVols.fxml");
+    }
+
+    @FXML
+    private void showPassagersPage() {
+        loadPage("pagePassagers.fxml");
+    }
+
+    @FXML
+    private void showAdminsPage() {
+        loadPage("pageAdmins.fxml");
+    }
+
+    private void loadPage(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/" + fxmlFile));
+            Parent newPage = loader.load();
+            contentPane.getChildren().clear();
+            contentPane.getChildren().add(newPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
